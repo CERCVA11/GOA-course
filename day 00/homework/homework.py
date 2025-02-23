695adb1606f6742c02f2f80adfad775cabcc365b
CERCVA11 <s.tsertsvadze.game@gmail.com>
2025-02-23 18:43:55 +0400

    first commit

---
+++ day 00/homework/homework.py
@@ -0,0 +1,70 @@
+from turtle import *
+
+
+#we want to paint a house 
+
+#step 1:  draw a square
+
+from turtle import *
+
+
+#we want to paint a house
+
+
+#step 1: draw a square
+speed(30)
+width(7)
+color("purple")
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+#end of square
+
+#drawing a door
+
+forward(70)
+begin_fill()
+color("yellow")
+left(90)
+forward(120)       #height of the door
+right(90)
+forward(60)
+right(90)
+forward(120)
+end_fill()
+
+
+
+penup()
+goto(200, 200)
+begin_fill()
+pendown()
+color("grey")
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+
+
+
+exitonclick()
+
+
+
+
+
+
+
+
+
+
+
